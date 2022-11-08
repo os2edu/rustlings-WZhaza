3b369fbe5f8be8d70df27beae820c7103c5aa612
wzhaza <wzhaza@outlook.com>
2022-11-08 19:03:36 +0800

    第六次，87/94

--- exercises/standard_library_types/iterators4.rs
+++ exercises/standard_library_types/iterators4.rs
@@ -1,7 +1,6 @@
 // iterators4.rs
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
@@ -13,6 +12,22 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+
+    // let mut result=1_u64;
+    // loop{
+    //     if num>1{
+    //         result=result*num*(num-1);
+    //         num-=2;
+    //     }else{
+    //         break;
+    //     }
+    // }
+    // result
+
+    let range=1..num+1;
+    let result=range.fold(1,|acc,n| acc*n);
+
+    result
 }
 
 #[cfg(test)]
